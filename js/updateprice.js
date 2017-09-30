01dedf454d1c668985756e7dfc5b3837ecafc5f6
Raghav Garg <Raghav.Garg@paytm.com>
2017-09-30 15:02:21 +0530

    eth price

--- js/updateprice.js
+++ js/updateprice.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 $.get( "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD", function( data ) {
   $( ".ethprice" ).html( data["USD"] );
-  alert( data );
+});
+
+$.get( "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD", function( data ) {
+  $( ".btcprice" ).html( data["USD"] );
+});
+
+
+$.get( "https://min-api.cryptocompare.com/data/price?fsym=XRP&tsyms=USD", function( data ) {
+  $( ".xrpprice" ).html( data["USD"] );
 });
 });
